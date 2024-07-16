d93f008e39c7efb155b26b44e7e7127d346adbfb
Kulpot <kulpotwahu@gmail.com>
2024-07-16 18:15:23 +0800

    ...

--- SimonMemoryGame/SimonGame.cs
+++ SimonMemoryGame/SimonGame.cs
@@ -33,6 +33,7 @@ namespace SimonMemoryGame
                 {
                     guessPosition = 0;
                     generateColor();
+                    return false;
                 }
             }
         }
