cea8bd04a787433d1e4338e389b4d9f3a42389b5
Kulpot <kulpotwahu@gmail.com>
2024-07-19 19:54:14 +0800

    ...

--- SimonMemoryGame/Program.cs
+++ SimonMemoryGame/Program.cs
@@ -48,7 +48,7 @@ namespace SimonmemoryGame
                 Console.Clear();
                 Console.ResetColor();   // for Console.ForegroundColor not changing
             }
-            Console.WriteLine("Round " + game.Colors.Count + ": \n"); 
+            Console.WriteLine("Round " + game.Colors.Count + ": \n"); // Round Count = next round
 
 
         }
