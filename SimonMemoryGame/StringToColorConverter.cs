08c19b018f053691ff14c06283b46d0d0109e394
Kulpot <kulpotwahu@gmail.com>
2024-07-21 21:16:02 +0800

    case "yellow":                     break;

--- SimonMemoryGame/StringToColorConverter.cs
+++ SimonMemoryGame/StringToColorConverter.cs
@@ -18,6 +18,8 @@ namespace SimonMemoryGame
                     break;
                 case "green":
                     break;
+                case "red":
+                    break;
 
             }
         }
