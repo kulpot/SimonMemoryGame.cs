3c22819d91bd75a2bc115089eed93550380651a0
Kulpot <kulpotwahu@gmail.com>
2024-07-22 17:21:27 +0800

    ...

--- SimonMemoryGame/StringToColorConverter.cs
+++ SimonMemoryGame/StringToColorConverter.cs
@@ -21,7 +21,7 @@ namespace SimonMemoryGame
                 case "red":
                     return Colors.Red;
                 default:
-                    return Colors.Unknown;
+                    return Colors.Unknown;  // add to Color enumeration, Colors.cs
             }
         }
     }
